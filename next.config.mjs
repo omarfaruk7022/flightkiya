bdad0dceed31220c0d9fd1706383d9299d8054c2
omarfaruk7022 <farhanfaruk221@gmail.com>
2024-11-28 20:49:40 +0600

    push

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['fk-api.adbiyas.com'], // Just the domain without 'https://'
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ["fk-api.adbiyas.com"], // Just the domain without 'https://'
+  },
+  reactStrictMode: true,
+};
+
+export default nextConfig;
